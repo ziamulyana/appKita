9e3d1fa18f7a1e06d83a11c2310ffb9434a222e3
AgungBK <38200152+AgungBK@users.noreply.github.com>
2021-04-03 11:47:36 +0700

    Add files via upload

---
+++ application/controllers/Auth.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function index() {
+		$this->load->view('login');
+	}
+
+	public function cek_login() {
+		$data = array('username' => $this->input->post('username', TRUE),
+						'password' => md5($this->input->post('password', TRUE))
+			);
+		$this->load->model('model_user'); // load model_user
+		$hasil = $this->model_user->cek_user($data);
+		if ($hasil->num_rows() == 1) {
+			foreach ($hasil->result() as $sess) {
+				$sess_data['logged_in'] = 'Sudah Loggin';
+				$sess_data['id_user'] = $sess->id_user;
+				$sess_data['username'] = $sess->username;
+				$sess_data['level'] = $sess->level;
+				$this->session->set_userdata($sess_data);
+			}
+			if ($this->session->userdata('level')=='admin') {
+				redirect('admin/c_admin');
+			}
+			elseif ($this->session->userdata('level')=='pegawai') {
+				redirect('pegawai/c_pegawai');
+			}		
+		}
+		else {
+			echo "<script>alert('Gagal login: Cek username, password!');history.go(-1);</script>";
+		}
+	}
+
+}
+
+?>
